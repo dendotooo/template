597beee2fcc6cbe6a6a45e2d93728cf44202fd43
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-12-17 13:27:16 +0100

    refactor: update den.ooo to `v1.0.1`

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-import { createProxy } from 'proxy'
+import { Registry } from 'den.ooo'
+import { GitHub, GitLab } from 'den.ooo/resolvers'
 
-createProxy()
+const registry = new Registry({
+  domain: 'den.ooo', // < replace "den.ooo" with the domain of your app
+  resolvers: [
+    GitHub,
+    GitLab
+  ]
+})
+
+registry.serve()
